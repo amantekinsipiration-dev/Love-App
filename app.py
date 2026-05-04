8c223b8890610ad2dc3d853a07837640251d3357
amantekinsipiration-dev <amantekinsipiration@gmail.com>
2026-05-05 02:48:45 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Love App ❤️")
+
+name = st.text_input("Enter key (Your Name)")
+
+if st.button("Touch me ❤️"):
+    st.success(f"Hii, {name} bubu, I am Chhaya's Heart ❤️ I love youuuuu 🥰🥰🥰🥰🥰🥰❤️❤️❤️❤️❤️❤️❤️❤️")
