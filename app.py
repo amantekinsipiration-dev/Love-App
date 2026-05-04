04f78f6571728c41de29a16a7e6ee3fc95e79c86
amantekinsipiration-dev <amantekinsipiration@gmail.com>
2026-05-05 02:53:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("Love App ❤️")
 
-name = st.text_input("Enter key (Your Name)")
+name = st.text_input("Enter key (Your Name 🥰)")
 
 if st.button("Touch me ❤️"):
     st.success(f"Hii, {name} bubu, I am Chhaya's Heart ❤️ I love youuuuu 🥰🥰🥰🥰🥰🥰❤️❤️❤️❤️❤️❤️❤️❤️")
